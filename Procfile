9b9e11a9e465b6b9d56bda2a5e6fca48835f32b2
ItsAllDari <itsalldari@gmail.com>
2020-08-12 09:33:50 -0400

    Added files for deployment

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn miraculous-destinations.wsgi
+release: python manage.py migrate
--- miraculous-destinations/settings.py
+++ miraculous-destinations/settings.py
@@ -72,6 +72,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.whitenoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'miraculous-destinations.urls'
@@ -152,5 +153,6 @@ USE_TZ = True
 # optional package: http://whitenoise.evans.io/en/stable/django.html
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = 'api.User'
